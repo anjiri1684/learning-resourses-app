bebd6351a04a13f647421af90b832bd7f3ab1bb7
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-29 10:09:05 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      >{{
+    }}</learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningRosource.vue';
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 1,
+          title: 'Official Guide',
+          description: 'The official Vuejs documentation',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 2,
+          title: 'Vuejs Tutorial',
+          description: 'A tutorial for Vuejs',
+          link: 'https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/21526316#questions',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/learning-resources/LearningRosource.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p></p>
+    <nav>
+      <a href="">View Resource</a>
+    </nav>
+  </li>
+</template>
